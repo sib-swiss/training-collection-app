69451dab1725a9e0b89fcfc23b5a7256dd2928bd
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2022-08-24 09:17:10 +0200

    santizes code and adds comments

--- github_api.R
+++ github_api.R
@@ -1,12 +1,9 @@
 library(httr)
 library(stringr)
-# 
-# system(paste0(". ", getwd(), "/.env"))
-# 
-# write(Sys.getenv("GITHUB_USER"), stderr())
 
 source(".env.R")
 
+# function to interact with github api
 github_api <- function(path, config = list()) {
   url <- httr::modify_url("https://api.github.com", path = path)
   resp <- httr::GET(url, config = config)
@@ -26,15 +23,17 @@ github_api <- function(path, config = list()) {
   )
 }
 
+# pretty printing an api response
 print.github_api <- function(x, ...) {
   cat("<GitHub ", x$path, ">\n", sep = "")
   str(x$content)
   invisible(x)
 }
 
+# function for easy linebreaks in app
 linebreaks <- function(n){HTML(strrep(br(), n))}
 
-
+# return empty if null
 empty_if_null <- function(x){
   if(!is.null(x)){
     return(x)
@@ -42,6 +41,7 @@ empty_if_null <- function(x){
   return("")
 }
 
+# create link out of character if not NULL nor empty
 to_link <- function(x){
   if(is.null(x)) {
     return("")
@@ -58,6 +58,7 @@ to_link <- function(x){
   
 }
 
+# convert slug to github link
 slug_to_link <- function(slug){
   link <- paste0("https://github.com/", slug)
   html <- sprintf('<a href="%s" target="_blank">%s</a>',
@@ -65,13 +66,54 @@ slug_to_link <- function(slug){
   return(html)
 }
 
+# pretty format stargazers
 format_stargazers <- function(stargazers_count){
   sort_string <- formatC(stargazers_count, width = 12,
                          format = "d", flag = "0")
   star_icon <- icon("star", lib = "glyphicon", style="color:#f1c40f")
-  stargazers_string <- paste("<!--", sort_string, "-->", as.character(star_icon),
+  stargazers_string <- paste("<!--", sort_string, "-->", 
+                             as.character(star_icon),
                              stargazers_count)
   return(stargazers_string)
+}
+
+# convert list of responses into dataframe
+get_repo_info_df <- function(responses){
+  
+  df_list <- list()
+  for(response in responses){
+    repo_record <- response$content
+    timediff_latest_push <- Sys.time() - as.POSIXct(repo_record$pushed_at)
+    days_since_latest_push <- round(as.numeric(timediff_latest_push, 
+                                               units = "days"))
+    df <- data.frame(
+      Repository = slug_to_link(repo_record$full_name),
+      Description = empty_if_null(repo_record$description),
+      Website = to_link(repo_record$homepage),
+      Stargazers = format_stargazers(repo_record$stargazers_count),
+      Days_since_last_push = days_since_latest_push,
+      License = empty_if_null(repo_record$license$key)
+    )
+    df_list[[repo_record$full_name]] <- df
+  }
+  
+  repo_df <- do.call(rbind, df_list)
+  repo_df <- repo_df[order(repo_df$Stargazers, decreasing = TRUE),]
+  colnames(repo_df) <- gsub("_", " ", colnames(repo_df))
+  return(repo_df)
+}
+
+# 1:1 mapping of slugs-tags
+get_tag_df <- function(slugs_tags_df){
+  tag_list <- list()
+  for(slug in rownames(slugs_tags_df)){
+    tags <- strsplit(slugs_tags_df[slug, "tags"], ",")[[1]]
+    tags <- str_trim(tags)
+    tag_list[[slug]] <- data.frame(tag = tags, slug = slug)
+  }
+  
+  tag_df <- do.call(rbind, tag_list)
+  return(tag_df)
 }
 
 main <- function(){
@@ -83,54 +125,34 @@ main <- function(){
                       units = "hours")
   }
   
-  if(age > 1 | !file.exists("repo_info.rds")){
+  # only recreate if repo_info.rds is older than 1 day
+  if(age > 24 | !file.exists("repo_info.rds")){
     
-    repo_slugs <- read.delim("gh_slugs_tags.txt", header = FALSE)
-    colnames(repo_slugs) <- c("slug", "tags")
-    repo_slugs <- repo_slugs[!duplicated(repo_slugs$slug),]
-    rownames(repo_slugs) <- repo_slugs$slug
+    # read in tab delimited file with slugs and tags
+    slugs_tags_df <- read.delim("gh_slugs_tags.txt", header = FALSE)
+    colnames(slugs_tags_df) <- c("slug", "tags")
+    slugs_tags_df <- slugs_tags_df[!duplicated(slugs_tags_df$slug),]
+    rownames(slugs_tags_df) <- slugs_tags_df$slug
     
-    tag_list <- list()
-    for(slug in rownames(repo_slugs)){
-      tags <- strsplit(repo_slugs[slug, "tags"], ",")[[1]]
-      tags <- str_trim(tags)
-      tag_list[[slug]] <- data.frame(tag = tags, slug = slug)
-    }
-    
-    tag_df <- do.call(rbind, tag_list)
-    
-    responses <- lapply(repo_slugs$slug, function(repo_slug){
+    # get api resonses of repo info
+    responses <- lapply(slugs_tags_df$slug, function(repo_slug){
       github_api(paste0("/repos/", repo_slug), 
                  authenticate(user = GITHUB_USER,
                               password = GITHUB_PAT))
     }) 
     
-    df_list <- list()
-    for(response in responses){
-      repo_record <- response$content
-      timediff_latest_push <- Sys.time() - as.POSIXct(repo_record$pushed_at)
-      days_since_latest_push <- round(as.numeric(timediff_latest_push, 
-                                                 units = "days"))
-      df <- data.frame(
-        Repository = slug_to_link(repo_record$full_name),
-        Description = empty_if_null(repo_record$description),
-        Website = to_link(repo_record$homepage),
-        Stargazers = format_stargazers(repo_record$stargazers_count),
-        Days_since_last_push = days_since_latest_push,
-        License = empty_if_null(repo_record$license$key)
-      )
-      df_list[[repo_record$full_name]] <- df
-    }
+    # convert responses into dataframe with records of interest
+    repo_df <- get_repo_info_df(responses)
     
-    repo_df <- do.call(rbind, df_list)
-    repo_df <- repo_df[order(repo_df$Stargazers, decreasing = TRUE),]
-    colnames(repo_df) <- gsub("_", " ", colnames(repo_df))
+    # convert tags into 1:1 tag - slug mapping
+    tag_df <- get_tag_df(slugs_tags_df)
     
-    
+    # return repo info and tag 
     repo_info <- list(repo_df = repo_df, tag_df = tag_df)
     saveRDS(repo_info, "repo_info.rds")
   }
   
+  # read file from disk
   repo_info <- readRDS("repo_info.rds")
   return(repo_info)
 }
